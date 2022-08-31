a884509222e78df4f0d31e9bcfc9a389f7d815b4
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-09-01 00:40:54 +0300

    print no subtype if invalid subtype is entered

--- commands/socionics.js
+++ commands/socionics.js
@@ -176,66 +176,82 @@ module.exports = {
             case "ile":
                 if(subtype == "ti") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
                 else if(subtype == "ne") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('ILE Profile By Gulenko').setDescription(socionics.filter(element => element.name == "ILE.txt")[0].content)],})
             case "sei":
-                if(subtype == "si") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "fe") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "si") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "SEI-Si.txt")[0].content)],})
+                else if(subtype == "fe") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "SEI-Fe.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('SEI Profile By Gulenko').setDescription(socionics.filter(element => element.name == "SEI.txt")[0].content)],})
             case "ese":
-                if(subtype == "fe") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "si") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "fe") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ESE-Fe.txt")[0].content)],})
+                else if(subtype == "si") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ESE-Si.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('ESE Profile By Gulenko').setDescription(socionics.filter(element => element.name == "ESE.txt")[0].content)],})
             case "lii":
-                if(subtype == "ti") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "ne") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "ti") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "LII-Ti.txt")[0].content)],})
+                else if(subtype == "ne") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "LII-Ne.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('LII Profile By Gulenko').setDescription(socionics.filter(element => element.name == "LII.txt")[0].content)],})
             case "sle":
-                if(subtype == "se") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "ti") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "se") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "SLE-Se.txt")[0].content)],})
+                else if(subtype == "ti") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "SLE-Ti.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('SLE Profile By Gulenko').setDescription(socionics.filter(element => element.name == "SLE.txt")[0].content)],})
             case "iei":
-                if(subtype == "ni") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "fe") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "ni") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "IEI-Ni.txt")[0].content)],})
+                else if(subtype == "fe") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "IEI-Fe.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('IEI Profile By Gulenko').setDescription(socionics.filter(element => element.name == "IEI.txt")[0].content)],})
             case "eie":
-                if(subtype == "fe") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "ni") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "fe") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "EIE-Fe.txt")[0].content)],})
+                else if(subtype == "ni") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "EIE-Ni.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('EIE Profile By Gulenko').setDescription(socionics.filter(element => element.name == "EIE.txt")[0].content)],})
             case "lsi":
-                if(subtype == "ti") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "se") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "ti") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "LSI-Ti.txt")[0].content)],})
+                else if(subtype == "se") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "LSI-Se.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('LSI Profile By Gulenko').setDescription(socionics.filter(element => element.name == "LSI.txt")[0].content)],})
             case "see":
-                if(subtype == "se") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "fi") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "se") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "SEE-Se.txt")[0].content)],})
+                else if(subtype == "fi") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "SEE-Fi.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('SEE Profile By Gulenko').setDescription(socionics.filter(element => element.name == "SEE.txt")[0].content)],})
             case "ili":
-                if(subtype == "ni") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "te") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "ni") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILI-Ni.txt")[0].content)],})
+                else if(subtype == "te") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILI-Te.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('ILI Profile By Gulenko)').setDescription(socionics.filter(element => element.name == "ILI.txt")[0].content)],})
             case "lie":
-                if(subtype == "te") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "ni") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "te") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "LIE-Te")[0].content)],})
+                else if(subtype == "ni") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "LIE-NNi.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('LIE Profile By Gulenko)').setDescription(socionics.filter(element => element.name == "LIE.txt")[0].content)],})
             case "esi":
-                if(subtype == "fi") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "se") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "fi") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ESI-Fi.txt")[0].content)],})
+                else if(subtype == "se") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ESI-Se.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('ESI Profile By Gulenko)').setDescription(socionics.filter(element => element.name == "ESI.txt")[0].content)],})
             case "iee":
-                if(subtype == "ne") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "fi") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "ne") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "IEE-Ne.txt")[0].content)],})
+                else if(subtype == "fi") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "IEE-Fi.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('IEE Profile By Gulenko)').setDescription(socionics.filter(element => element.name == "IEE.txt")[0].content)],})
             case "sli":
-                if(subtype == "si") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "te") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "si") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "SLI-Si.txt")[0].content)],})
+                else if(subtype == "te") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "SLI-Te.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('SLI Profile By Gulenko)').setDescription(socionics.filter(element => element.name == "SLI.txt")[0].content)],})
             case "lse":
-                if(subtype == "te") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "si") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "te") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "LSE-Te.txt")[0].content)],})
+                else if(subtype == "si") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "LSE-Si.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('LSE Profile By Gulenko)').setDescription(socionics.filter(element => element.name == "LSE.txt")[0].content)],})
             case "eii":
-                if(subtype == "fi") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ti.txt")[0].content)],})
-                else if(subtype == "ne") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "ILE-Ne.txt")[0].content)],})
+                if(subtype == "fi") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "EII-Fi.txt")[0].content)],})
+                else if(subtype == "ne") return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('1w9 (The Idealist)').setDescription(socionics.filter(element => element.name == "EII-Ne.txt")[0].content)],})
+                else if(subtype) return interaction.reply('no subtype');
                 else return interaction.reply({embeds: [EmbedBuilder.from(embed).setTitle('EII Profile By Gulenko)').setDescription(socionics.filter(element => element.name == "EII.txt")[0].content)],})
         }
 
