bccfa48d799a03fc40dcb1cbfb6f87d4abccf6eb
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-09-03 14:36:40 +0300

    all commands import embed from embed.js

--- commands/tritype.js
+++ commands/tritype.js
@@ -15,26 +15,9 @@ for (const file of tritypeFiles) {
     })
 }
 
-/* Embed template */
-const { EmbedBuilder } = require('discord.js');
+//require embed
+const {embed} = require("../embed.js")
 
-const embed = new EmbedBuilder()
-	.setColor("#f0f018")
-	.setTitle('1w2 (The Advocate)')
-	//.setURL('https://www.linkedin.com/in/bahadır-ustabaşı-693550165/')
-	//.setAuthor({ name: 'Some name', iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org' })
-	//.setDescription(enneagrams.filter(element => element.name == "1w2.txt")[0].content)
-	//.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-	/*.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)*/
-	//.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-	//.setImage('https://i.imgur.com/AfFp7pu.png')
-	//.setTimestamp()
-	.setFooter({ text: 'This bot is made by Bahadır Ustabaşı', /*iconURL: 'https://i.imgur.com/AfFp7pu.png'*/ });
 
 module.exports = {
 	data: new SlashCommandBuilder()
