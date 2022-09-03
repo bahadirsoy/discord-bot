81f0b52f2ec594bccd0e4cc0b9d6ee541af9617e
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-09-03 14:57:05 +0300

    help command description update

--- commands/help.js
+++ commands/help.js
@@ -6,12 +6,14 @@ var fs = require('fs')
 //require embed
 const {embed} = require("../embed.js")
 
+var description = "asdsa";
+description = fs.readFileSync(`/app/helpcommanddesc.txt`).toString()
 
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
 		.setDescription('Typology Bot Guide'),
 	async execute(interaction) {
-		return interaction.reply({embeds: [embed.setTitle("Typology Bot")],})
+		return interaction.reply({embeds: [embed.setTitle("Typology Bot").setDescription(description)],})
 	},
 };
--- embed.js
+++ embed.js
@@ -4,16 +4,13 @@ const { EmbedBuilder } = require('discord.js');
 //get help description
 var fs = require('fs')
 
-var description = "asdsa";
-description = fs.readFileSync(`/app/helpcommanddesc.txt`).toString()
-
 
 const embed = new EmbedBuilder()
 	.setColor("#f0f018")
 	.setTitle('default title')
 	//.setURL('https://www.linkedin.com/in/bahadır-ustabaşı-693550165/')
 	//.setAuthor({ name: 'Typology Bot', /*iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org'*/ })
-	.setDescription(description)
+	.setDescription("default description")
 	//.setThumbnail('https://i.imgur.com/AfFp7pu.png')
 	/*.addFields(
 		{ name: 'Regular field title', value: 'Some value here' },
