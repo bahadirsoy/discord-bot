e47edcf233a01ea51fb5302e2e47ee53ba05f053
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-09-03 14:31:06 +0300

    embed.js bug fix

--- commands/help.js
+++ commands/help.js
@@ -7,24 +7,6 @@ var fs = require('fs')
 const {embed} = require("../embed.js")
 
 
-const embed = new EmbedBuilder()
-	.setColor("#f0f018")
-	.setTitle('Typology Bot Commands')
-	//.setURL('https://www.linkedin.com/in/bahadır-ustabaşı-693550165/')
-	.setAuthor({ name: 'Typology Bot', /*iconURL: 'https://i.imgur.com/AfFp7pu.png', url: 'https://discord.js.org'*/ })
-	.setDescription(description)
-	//.setThumbnail('https://i.imgur.com/AfFp7pu.png')
-	/*.addFields(
-		{ name: 'Regular field title', value: 'Some value here' },
-		{ name: '\u200B', value: '\u200B' },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-		{ name: 'Inline field title', value: 'Some value here', inline: true },
-	)*/
-	//.addFields({ name: 'Inline field title', value: 'Some value here', inline: true })
-	//.setImage('https://i.imgur.com/AfFp7pu.png')
-	//.setTimestamp()
-	.setFooter({ text: 'This bot is made by Bahadır Ustabaşı', /*iconURL: 'https://i.imgur.com/AfFp7pu.png'*/ });
-
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('help')
