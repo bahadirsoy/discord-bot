9ed487e41de297a38de505f1f7045556624c2a43
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-09-05 13:45:47 +0300

    guild name printed

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -2,6 +2,6 @@ module.exports = {
 	name: 'interactionCreate',
 	execute(interaction) {
 		console.log(`${interaction.user.tag} in #${interaction.channel.name} triggered an ${interaction.guild} interaction.`);
-		console.log(`\nInteraction is created by: ${interaction.user.tag}\nChannel name: ${interaction.channel.name}\nCreated at: ${interaction.createdAt}\n`);
+		console.log(`\nInteraction is created by: ${interaction.user.tag}\nChannel name: ${interaction.channel.name}\nCreated at: ${interaction.createdAt}\nGuild Name: ${interaction.member.guild.name}`);
 	},
 };
