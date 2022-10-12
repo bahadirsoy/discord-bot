e6eb64fa30dcb6938e35e33ec469b45a18e7f89f
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-10-12 23:33:06 +0300

    server count is now shown in activity but logged in console

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		client.user.setActivity(`/help in ${client.guilds.cache.size} servers`);
-		console.log(`Ready! Logged in as ${client.user.tag}`);
+		client.user.setActivity(`/help`);
+		console.log(`Ready! Logged in as ${client.user.tag}, Server count = ${client.guilds.cache.size}`);
 	},
 };
