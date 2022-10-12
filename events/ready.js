e12b58372602e1257554f9cd13ec5de8199856dc
Bahadir Ustabasi <oxikairos@hotmail.com>
2022-10-12 23:30:46 +0300

    server count is shown in activity

--- events/ready.js
+++ events/ready.js
@@ -2,7 +2,7 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	execute(client) {
-		client.user.setActivity(`/help`);
+		client.user.setActivity(`/help in ${client.guilds.cache.size} servers`);
 		console.log(`Ready! Logged in as ${client.user.tag}`);
 	},
 };
